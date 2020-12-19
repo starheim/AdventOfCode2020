172471f07eb34d5c6d2192857cc777798ebba5f8
William <wstarvall@gmail.com>
2020-12-19 13:01:02 +0100

    Day4 work

--- Day4/Part1.py
+++ Day4/Part1.py
@@ -1,21 +1,23 @@
 class Passport:
+    filledFields = 0
 
-	byr = None
-	iyr = None
-	eyr = None
-	hgt = None
-	hcl = None
-	pid = None
-	cid = None
+    content = {}
 
-fileReader = open("Day4/Day4.txt", "r")
+    byr = None
+    iyr = None
+    eyr = None
+    hgt = None
+    hcl = None
+    ecl = None
+    pid = None
+    cid = None
+
+fileReader = open("Day4/Test.txt", "r")
 textLines = fileReader.readlines()
 fileReader.close()
 
 numberOfPassports = 0
 numberOfValidPassports = 0
-
-validPassport = False
 
 passports = []
 passport = Passport()
@@ -23,32 +25,24 @@ passport = Passport()
 for s in textLines:
     if(s != '\n'):
         fields = s.split()
-        print(s[:-1])
-        print(fields)
         for field in fields:
-            print(field)
-            entry = field.split(:)
-            if entry[0] == 'byr'
-                passport.byr == entry[1]
-            else if entry[0] == 'iyr'
-                passport.iyr == entry[1]
-            else if entry[0] == 'eyr'
-                passport.eyr == entry[1]
-            else if entry[0] == 'hgt'
-                passport.iyr == entry[1]
-            else if entry[0] == 'hcl'
-                passport.iyr == entry[1]
-            else if entry[0] == 'ecl'
-                passport.iyr == entry[1]
-            else if entry[0] == 'pid'
-                passport.iyr == entry[1]
-            else if entry[0] == 'cid'
-                passport.iyr == entry[1]
+            entry = field.split(":")
+            passport.content[entry[0]] = entry[1]
+            passport.filledFields += 1
     else:
         numberOfPassports += 1
         passports.append(passport)
-        print(s)
-        print("-------------------------------------------------")
+        passport = Passport()
 
-print("Number of passports: {}".format(numberOfPassports))
+numberOfPassports += 1
+passports.append(passport)
+
+
+
+for p in passports:
+    print(p.filledFields)
+    print(p.content)
+    
+
+print("Number of passports: {}, number of valid ones: {}".format(numberOfPassports, numberOfValidPassports))
         
