4bb2b4b2f847a87ce7027a15b9975b2b10841e24
William <wstarvall@gmail.com>
2020-12-23 16:43:41 +0100

    Day6 complete

--- Day6/Part2.py
+++ Day6/Part2.py
@@ -1,17 +1,24 @@
+from string import ascii_lowercase
+
 fileReader = open("Day6/Day6.txt", "r")
 textLines = fileReader.readlines()
 fileReader.close()
 
 groupInfo = ""
 uniqueCountSum = 0
+groupSize = 0
 
 for line in textLines:
     if line != "\n":
         print(line.rstrip())
         groupInfo += line.rstrip()
+        groupSize += 1
     else:
-        uniqueCountSum += len(set(groupInfo))
-        print("String: {}, number of unique characters: {}\n".format(groupInfo, len(set(groupInfo))))
+        for a in ascii_lowercase:
+            if groupInfo.count(a) == groupSize:
+                uniqueCountSum += 1
+
         groupInfo = ""
+        groupSize = 0
 
 print(uniqueCountSum)
