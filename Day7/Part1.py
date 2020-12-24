aa139bdbd126fadf951324628d3c6f1c7f03b960
William <wstarvall@gmail.com>
2020-12-24 13:18:15 +0100

    Day7 part1 complete

--- Day7/Part1.py
+++ Day7/Part1.py
@@ -10,11 +10,9 @@ def contains(bag):
         lineInfo = line.split(" bags contain ")
         if bag in lineInfo[1].rstrip():
             newBags.append(lineInfo[0])
-            print(f"Searching {lineInfo[0]} recursively.")
             contains(lineInfo[0])
     bagsContainingShinyGoldBag.extend(newBags)
     
 contains("shiny gold")
 
 print(f"\nNumber of times shiny gold bag is contained in another bag: {len(set(bagsContainingShinyGoldBag))}")
-print(f"Bags containing shiny gold bag: {set(bagsContainingShinyGoldBag)}")
