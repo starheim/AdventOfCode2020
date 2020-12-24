b133af5597b2ee44017193e68bc5d2961a182965
William <wstarvall@gmail.com>
2020-12-24 13:03:20 +0100

    Day7 part1 complete

--- Day7/Part1.py
+++ Day7/Part1.py
@@ -1,22 +1,20 @@
-fileReader = open("Day7/Test.txt", "r")
+fileReader = open("Day7/Day7.txt", "r")
 textLines = fileReader.readlines()
 fileReader.close()
 
 bagsContainingShinyGoldBag = []
 
-def bagsContainingBag(bag):
+def contains(bag):
     newBags = []
     for line in textLines:
-        lineInfo = line.split(" contain ")
+        lineInfo = line.split(" bags contain ")
         if bag in lineInfo[1].rstrip():
             newBags.append(lineInfo[0])
+            print(f"Searching {lineInfo[0]} recursively.")
+            contains(lineInfo[0])
     bagsContainingShinyGoldBag.extend(newBags)
-    print(f"Found these bags, calling method on: {newBags}")
-    for bags in newBags:
-        print(f"Called on: {bags}")
-        bagsContainingBag(bags)
-
-bagsContainingBag("shiny gold bag")
+    
+contains("shiny gold")
 
 print(f"\nNumber of times shiny gold bag is contained in another bag: {len(set(bagsContainingShinyGoldBag))}")
 print(f"Bags containing shiny gold bag: {set(bagsContainingShinyGoldBag)}")
