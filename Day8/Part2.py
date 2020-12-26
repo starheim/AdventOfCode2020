cf2aca6d0421e2828f8addc0318756427d825ab2
William <wstarvall@gmail.com>
2020-12-26 01:58:43 +0100

    Day8 part 2 work in progress

---
+++ Day8/Part2.py
@@ -0,0 +1,28 @@
+textLines = open("Day8/Day8.txt", "r").readlines()
+
+accumulator = 0
+lineNumber = 0
+linesVisited = []
+linesChanged = []
+hasTerminated = False
+
+while not hasTerminated:
+
+    while lineNumber < len(textLines):
+        line = textLines[lineNumber]
+        lineInfo = line.rstrip().split(' ')
+        argument = int(lineInfo[1])
+        
+        if lineNumber in linesVisited:
+            print(f"Accumulator at start of infinite loop: {accumulator}")
+            break
+        
+        linesVisited.append(lineNumber)
+    
+        if lineInfo[0] == 'acc':
+            accumulator += argument
+            lineNumber += 1
+        elif lineInfo[0] == 'jmp':
+            lineNumber += argument
+        else:
+            lineNumber += 1
