9d17d9eccd0864130ecdc549e8d0e9ff09ff7aab
William <wstarvall@gmail.com>
2020-12-26 13:13:33 +0100

    Day 8 complete

--- Day8/Part2.py
+++ Day8/Part2.py
@@ -5,24 +5,40 @@ lineNumber = 0
 linesVisited = []
 linesChanged = []
 hasTerminated = False
+changeMade = False
 
-while not hasTerminated:
+while lineNumber < len(textLines):
+    line = textLines[lineNumber]
+    lineInfo = line.rstrip().split(' ')
 
-    while lineNumber < len(textLines):
+    if lineNumber in linesVisited:
+        print(f"Infinite loop detected, starting over. \nLine number: {lineNumber}, accumulator value: {accumulator}")
+        accumulator = 0
+        lineNumber = 0
+        linesVisited = []
         line = textLines[lineNumber]
         lineInfo = line.rstrip().split(' ')
-        argument = int(lineInfo[1])
+        changeMade = False
         
-        if lineNumber in linesVisited:
-            print(f"Accumulator at start of infinite loop: {accumulator}")
-            break
-        
-        linesVisited.append(lineNumber)
-    
-        if lineInfo[0] == 'acc':
-            accumulator += argument
-            lineNumber += 1
-        elif lineInfo[0] == 'jmp':
+    instruction = lineInfo[0]
+    argument = int(lineInfo[1])
+    linesVisited.append(lineNumber)
+
+    if instruction == 'acc':
+        accumulator += argument
+        lineNumber += 1
+    elif instruction == 'jmp':
+        if not changeMade and lineNumber not in linesChanged:
+            linesChanged.append(lineNumber)
+            lineNumber +=1
+            changeMade = True
+        else:
+            lineNumber += argument
+    else:
+        if not changeMade and lineNumber not in linesChanged:
+            linesChanged.append(lineNumber)
             lineNumber += argument
         else:
             lineNumber += 1
+
+print(f"Boot code completed, accumulator: {accumulator}")
